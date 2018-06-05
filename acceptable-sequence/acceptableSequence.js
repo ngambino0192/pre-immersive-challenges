e9e6a9bba6e718c727baf29ad82dccac0c368e2c
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-05 11:40:33 -1000

    commit-1

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,23 @@
 module.exports = function acceptableSequence(str) {
-  // write your code here
+ 
+console.log(str);
+
+// var letters = "b";
+// console.log(letters);
+
+    for (i=0; i<str.length; i++){
+        var letters = /[a-z]/g;
+        var isLetter = letters.test(str[i]);
+        var result = "";
+        console.log(isLetter);
+        if (isLetter === "true" && str.charAt[i-1] === "+" && str.charAt[i+1] === "+"){
+            result = true;
+        } else{
+            result = false;
+        }
+        console.log(result);
+}
+str = result;
+return str;
+
 }
