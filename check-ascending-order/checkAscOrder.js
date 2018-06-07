473f99d870f5606a37809ad071def42d29c871de
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-07 00:34:42 -1000

    complete

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,16 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  console.log(numArray);
+
+  for (i=0; i<numArray.length-1; i++){
+    var result = "";
+    if (numArray[i] < numArray[i+1]){
+      result = true;
+    } else{
+      result = false;
+    }
+    console.log(result);
+    str = result;
+    return str;
+  }
 }
