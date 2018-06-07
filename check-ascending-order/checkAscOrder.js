521e31406764e976b8cecfb21c41550f931c9194
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-07 00:36:01 -1000

    complete with 'console.log' removed

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,7 +1,5 @@
 module.exports = function checkAscOrder(numArray) {
-  // write your code in here
-  console.log(numArray);
-
+  
   for (i=0; i<numArray.length-1; i++){
     var result = "";
     if (numArray[i] < numArray[i+1]){
@@ -9,7 +7,6 @@ module.exports = function checkAscOrder(numArray) {
     } else{
       result = false;
     }
-    console.log(result);
     str = result;
     return str;
   }
