4ed82f64072f6902b21bbd94a8b2a7d50d1e22ef
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-07 13:11:20 -1000

    complete

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,14 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
-}
+
+  var y = x;
+  var max = x * length;
+  var result = [];
+  
+  while (x<=max){
+    result.push(x);
+    x += y;
+  }
+  console.log(result);
+  return result;
+} 
