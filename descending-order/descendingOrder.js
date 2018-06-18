fafd6507c5d28a94d77a8fcdb3c22f3dd2430691
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-17 14:25:35 -1000

    descendingOrder

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,15 @@
 module.exports = function descendingOrder(number) {
   // write code in here
+
+  // console.log(number);
+  // console.log(typeof number);
+
+  
+  // console.log(number);
+  if (typeof number === 'string'){
+    return 'not a number!'
+  }else {
+    var numStr = number.toString();
+  }
+  return numStr.split('').sort().reverse().join('');
 }
