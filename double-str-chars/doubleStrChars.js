18b3584859b485355e312c306a0f76cef36d2093
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-17 14:42:00 -1000

    doubleStrChars

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,12 @@
 module.exports = function doubleStrChars(str) {
   // write your code in here
+  if (typeof(str) !== 'string'){
+    return 'not a string!';
+  }
+  var strArr = str.split('');
+  var myArray = [];
+  var double = strArr.forEach(function(element){
+    myArray.push(element + element);
+  });
+  return myArray.join('');
 }
