97c063157d3e0446e84515c878ab9c1930ad6bf6
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-07-06 21:53:15 -1000

    find middle value

--- find-middle-value/findMiddleValue.js
+++ find-middle-value/findMiddleValue.js
@@ -1,21 +1,20 @@
 module.exports = function findMiddleValue(numArray) {
   // write your code in here
-  // console.log(numArray);
 
-
-
-// console.log(numArray.sort());
-
-var newArr = [];
-
-for (i=0; i<numArray.length; i++){
-  if (numArray[i].length = 1){
-    newArr.push(numArray[i]);
-  }
-console.log(newArr);
-}
-
-
+  var originArr = [];
+  // save values of unsorted array into memory with forEach;
+  // you must do this so you can refer to the original unsorted array after it is mutated;
+  numArray.forEach(function(element){
+    originArr.push(element);
+    return numArray;
+  });
   
+  // sort array in ascending order, then locate the middle value;
+  var newArray = numArray.sort(function(a,b){ return (a - b);}); 
+  var middleValue = newArray[1];
+
+  // return the index of the middle arrays value within the origin array;
+  return originArr.indexOf(middleValue);
+
 
 }
